6f18ee2e25dbedcaa0f2a703779abbb8556a244b
vagrant <vagrant@localhost.localdomain>
2020-07-08 13:16:17 +0000

    [add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,7 +12,6 @@ class BooksController < ApplicationController
   end
 
   def new
-  	@book = Book.new
   end
 
   def edit
@@ -26,9 +25,13 @@ class BooksController < ApplicationController
   end
 
   def create
-  	book = Book.new(book_params)
-  	book.save
-  	redirect_to book_path(book.id)
+  	@book = Book.new(book_params)
+    if @book.save
+      redirect_to @book, notice: 'Book was successfully created.'
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def destroy
