519f0fe3b98189a2c3b41e359e5bba5f09acbce0
vagrant <vagrant@localhost.localdomain>
2020-07-09 06:30:04 +0000

    [finish]

--- Gemfile
+++ Gemfile
@@ -53,7 +53,7 @@ end
 group :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '>= 2.15'
-  gem 'respec-rails'
+  gem 'rspec-rails'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'factory_bot_rails'
   gem 'faker'
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -19,9 +19,12 @@ class BooksController < ApplicationController
   end
 
   def update
-  	book = Book.find(params[:id])
-  	book.update(book_params)
-  	redirect_to books_path
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+  	   redirect_to book_path(@book), notice: 'Book was successfully created.'
+    else
+      render :edit
+    end
   end
 
   def create
